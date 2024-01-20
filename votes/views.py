960c1ae090580ec67e3c3401b7ba69db26a300c8
Ila-bura <>
2024-01-20 10:13:10 +0000

    Create VoteList and VoteDetail generic views

--- votes/views.py
+++ votes/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from MewMes_API.permissions import IsOwnerOrReadOnly
+from votes.models import Vote
+from votes.serializers import VoteSerializer
 
-# Create your views here.
+class VoteList(generics.ListCreateAPIView):
+    """
+    List votes or create a vote if logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = VoteSerializer
+    queryset = Vote.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class VoteDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a vote or owner can delete it by id.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = VoteSerializer
+    queryset = Vote.objects.all()
