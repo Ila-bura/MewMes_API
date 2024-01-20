01a64ed348058657b59515e65d893f9a9d562dd7
Ila-bura <>
2024-01-20 09:06:31 +0000

    Add ReplyList and ReplyDetail generic views

--- reply/views.py
+++ reply/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from MewMes_API.permissions import IsOwnerOrReadOnly
+from .models import Reply
+from .serializers import ReplySerializer, ReplyDetailSerializer
 
-# Create your views here.
+class ReplyList(generics.ListCreateAPIView):
+    """
+    List replies or create a reply when logged in.
+    """
+    serializer_class = ReplySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Reply.objects.all()
+
+    """
+    Makes sure replies are associated with user upon creation.
+    """
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class ReplyDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Update, retrieve and delete a reply by the owner only.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = ReplyDetailSerializer
+    queryset = Reply.objects.all()
