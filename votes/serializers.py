aff3462d6660d18d189136ba730f671ecf5da852
Ila-bura <>
2024-01-20 09:19:46 +0000

    Create Votes app and related serializer

---
+++ votes/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from votes.models import Vote
+
+
+class VoteSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Vote model
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Vote
+        fields = ['id', 'created_at', 'owner', 'post']
